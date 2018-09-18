ec6bb723edcd08fb4e957a6e03f83423410d2579
lefedoreichmann <asta7799@gmail.com>
2018-09-18 17:14:12 +0900

    twitrend crawler

---
+++ twittertrendlist.py
@@ -0,0 +1,12 @@
+links =[
+"https://tr.twipple.jp/matome/",
+'https://tr.twipple.jp/entame/',
+'https://tr.twipple.jp/sports/',
+'https://tr.twipple.jp/society/',
+'https://tr.twipple.jp/politics/',
+'https://tr.twipple.jp/economy/',
+'https://tr.twipple.jp/world/',
+'https://tr.twipple.jp/it/',
+'https://tr.twipple.jp/game/',
+]
+
